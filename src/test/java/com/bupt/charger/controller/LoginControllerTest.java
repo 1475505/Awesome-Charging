f4ad23644b75e298d05ab43aaf02dec5af9dc60b
1475505 <liul2566@bupt.edu.cn>
2023-05-26 20:18:47 +0800

    fix test class

--- src/test/java/com/bupt/charger/controller/LoginControllerTest.java
+++ src/test/java/com/bupt/charger/controller/LoginControllerTest.java
@@ -1,48 +1,68 @@
 package com.bupt.charger.controller;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
-import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.net.URL;
+
 
 import static org.junit.jupiter.api.Assertions.*;
-
 /**
- * @author ll （ created: 2023-05-26 20:05 )
- */
+@author ll （ created: 2023-05-26 20:05 )
+*/
 class LoginControllerTest {
-    public static void main(String[] args) {
 
+    private static final String LOGIN_URL = "http://localhost:8080/login";
+    private static final String USERNAME = "testUser";
+    private static final String PASSWORD = "testPassword";
+
+    @Test
+    void test(){
+        testLogin(USERNAME, PASSWORD);
+    }
+    
+    void testLogin(String username, String password) {
         try {
-            // 请求的URL
-            URL url = new URL("http://localhost:8080/login");
-
-            // 创建连接
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-
             // 设置请求体参数
-            String username = "testUser";
-            String password = "testPassword";
             String requestBody = "username=" + username + "&password=" + password;
             byte[] requestBodyBytes = requestBody.getBytes(StandardCharsets.UTF_8);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Content-Length", String.valueOf(requestBodyBytes.length));
 
-            // 发送请求体数据
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(requestBodyBytes);
-            outputStream.flush();
-            outputStream.close();
-
-            // 获取响应
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            // 可以根据需要获取响应内容等
+            // 发送请求并获取响应
+            String responseBody = sendPostRequest(LOGIN_URL, requestBodyBytes);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private String sendPostRequest(String url, byte[] requestBodyBytes) throws Exception {
+        URL apiUrl = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Length", String.valueOf(requestBodyBytes.length));
+
+        try (OutputStream outputStream = connection.getOutputStream()) {
+            outputStream.write(requestBodyBytes);
+            outputStream.flush();
+        }
+
+        // 获取响应
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        // 读取响应内容
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            return response.toString();
         }
     }
 }
--- src/test/java/com/bupt/charger/controller/SignupControllerTest.java
+++ src/test/java/com/bupt/charger/controller/SignupControllerTest.java
@@ -1,5 +1,9 @@
 package com.bupt.charger.controller;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -9,39 +13,55 @@ import java.nio.charset.StandardCharsets;
  * @author ll （ created: 2023-05-26 20:01 )
  */
 class SignupControllerTest {
-    public static void main(String[] args) {
+    private static final String SIGNUP_URL = "http://localhost:8080/signup";
+    private static final String USERNAME = "testUser";
+    private static final String PASSWORD = "testPassword";
+    private static final String CAR_ID = "testCar_id";
 
+
+    @Test
+    void test(){
+        testLogin(USERNAME, PASSWORD, CAR_ID);
+    }
+
+    void testLogin(String username, String password, String car_id) {
         try {
-            // 请求的URL
-            URL url = new URL("http://localhost:8080/signup");
-
-            // 创建连接
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setDoOutput(true);
-
             // 设置请求体参数
-            String username = "testUser";
-            String password = "testPassword";
-            String car_id = "AF_1234";
             String requestBody = "username=" + username + "&password=" + password + "&car_id=" + car_id;
             byte[] requestBodyBytes = requestBody.getBytes(StandardCharsets.UTF_8);
-            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            connection.setRequestProperty("Content-Length", String.valueOf(requestBodyBytes.length));
 
-            // 发送请求体数据
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(requestBodyBytes);
-            outputStream.flush();
-            outputStream.close();
-
-            // 获取响应
-            int responseCode = connection.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
-
-            // 可以根据需要获取响应内容等
+            // 发送请求并获取响应
+            String responseBody = sendPostRequest(SIGNUP_URL, requestBodyBytes);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private String sendPostRequest(String url, byte[] requestBodyBytes) throws Exception {
+        URL apiUrl = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) apiUrl.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setDoOutput(true);
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        connection.setRequestProperty("Content-Length", String.valueOf(requestBodyBytes.length));
+
+        try (OutputStream outputStream = connection.getOutputStream()) {
+            outputStream.write(requestBodyBytes);
+            outputStream.flush();
+        }
+
+        // 获取响应
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        // 读取响应内容
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+            StringBuilder response = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                response.append(line);
+            }
+            return response.toString();
         }
     }
 }
