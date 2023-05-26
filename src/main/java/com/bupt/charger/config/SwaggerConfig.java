928b8624ca2a9831582bb30be604b90e5c702f6c
1475505 <liul2566@bupt.edu.cn>
2023-05-26 20:22:44 +0800

    fix SwaggerConfig

--- src/main/java/com/bupt/charger/config/SwaggerConfig.java
+++ src/main/java/com/bupt/charger/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.example.api.controllers"))
+                .apis(RequestHandlerSelectors.basePackage("com.bupt.charger.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
