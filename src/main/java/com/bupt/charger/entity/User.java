85e1bc4f7ca29f49a0b823cb8ce3a3e9914c0928
1475505 <liul2566@bupt.edu.cn>
2023-05-26 19:20:36 +0800

    init user entity

--- src/main/java/com/bupt/charger/entity/User.java
+++ src/main/java/com/bupt/charger/entity/User.java
@@ -1,10 +1,13 @@
 package com.bupt.charger.entity;
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NonNull;
 
 /**
  * @author ll （ created: 2023-05-26 19:03 )
  */
+@Data
 @Entity
 @Table(name = "users")
 public class User {
@@ -13,35 +16,18 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NonNull
     private String username;
 
+    @NonNull
     private String password;
 
     private String car_id;
 
-    // 构造函数、Getter和Setter方法等
+    public User() {}
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getCar_id() {
-        return car_id;
-    }
-
-    public void setCar_id(String car_id) {
-        this.car_id = car_id;
+    public User(String username, String password) {
+        setUsername(username);
+        setPassword(password);
     }
 }
