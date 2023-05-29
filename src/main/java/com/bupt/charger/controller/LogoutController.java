3352c42bc02f2ca86fcdb17174711f9fdddaa8a7
1475505 <liul2566@bupt.edu.cn>
2023-05-29 16:15:31 +0800

    fix: POST请求老老实实建request类吧

--- src/main/java/com/bupt/charger/controller/LogoutController.java
+++ src/main/java/com/bupt/charger/controller/LogoutController.java
@@ -2,6 +2,7 @@ package com.bupt.charger.controller;
 
 import com.bupt.charger.common.ApiResp;
 import com.bupt.charger.request.LoginRequest;
+import com.bupt.charger.request.LogoutRequest;
 import com.bupt.charger.response.UserLoginResponse;
 import com.bupt.charger.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 public class LogoutController {
 
     @PostMapping
-    public ResponseEntity<Object> logout(@RequestParam("username") String username) {
+    public ResponseEntity<Object> logout(@RequestBody LogoutRequest request) {
         try {
             return ResponseEntity.ok().body(new ApiResp(0, "请求成功"));
         } catch (Exception e) {
