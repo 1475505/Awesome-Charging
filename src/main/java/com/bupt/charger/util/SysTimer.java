433942d213b7d58dcda1e3c5795e36f2d5ecfd5f
archester-250 <76891850+archester-250@users.noreply.github.com>
2023-05-31 18:47:54 +0800

    :sparkles: Add Timer
    用法: SysTimer.getStartTime(); Return: Date()

---
+++ src/main/java/com/bupt/charger/util/SysTimer.java
@@ -0,0 +1,28 @@
+package com.bupt.charger.util;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.Timer;
+
+public class SysTimer {
+    private static Date startTime;
+    private Timer timer;
+
+    public void startClock()
+    {
+        Calendar calendar = Calendar.getInstance(); // 获取系统默认的Calendar实例
+        calendar.set(2023, Calendar.MAY, 31, 8, 0, 0); // 设置日期为2023年5月31日8点
+        SysTimer.startTime = calendar.getTime();//这里是从2023年5月31日8:00开始
+//        SysTimer.startTime = new Date();//这里是从当前时间开始
+        timer = new Timer();
+        timer.schedule(new SysTimerTask(),0, 1000);
+    }
+
+    public static Date getStartTime() {
+        return startTime;
+    }
+
+    public static void setStartTime(Date startTime) {
+        SysTimer.startTime = startTime;
+    }
+}
